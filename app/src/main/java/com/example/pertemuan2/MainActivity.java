692303b7f0c9194a66821cd31cf3b6660a55e0ef
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-06 19:04:06 +0700

    Update Main Activity - Membuat tampilan travel

--- app/src/main/java/com/example/pertemuan2/MainActivity.java
+++ app/src/main/java/com/example/pertemuan2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pertemuan2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
